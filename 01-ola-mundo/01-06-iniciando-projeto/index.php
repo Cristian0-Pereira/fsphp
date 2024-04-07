de797f0b8305ed3e8d0e02a2992db0faf73205c7
Cristian0-Pereira <cristianops2020@gmail.com>
2024-04-07 01:22:38 -0300

    ad

---
+++ 01-ola-mundo/01-06-iniciando-projeto/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FSPHP: Iniciando um Projeto</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php 
+/** 
+ * Bloco PHP com variável start
+ *   e comando de saída
+*/ 
+$start = "Vamos começar!";
+echo "<h1>Olá Mundo! {$start}</h1>";
+echo "<p id='js'>Loading...</p>";
+?>
+
+<script src="assets/scripts.js"></script>
+</body>
+</html>
