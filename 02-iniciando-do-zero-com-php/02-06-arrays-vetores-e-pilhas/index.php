fbf2b689e60840744f02ed57f2c5f737fd58a310
Cristian0-Pereira <cristianops2020@gmail.com>
2024-04-13 01:02:38 -0300

    add

--- 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
+++ 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
@@ -34,6 +34,7 @@ $arrayAssoc = [
     "bass-guitar" => "Cliff",
 ];
 $arrayAssoc["drums"] = "Phil";
+$arrayAssoc["rock-band"] = "AC/DC";
 
 var_dump($arrayAssoc);
 
@@ -42,9 +43,72 @@ var_dump($arrayAssoc);
  */
 fullStackPHPClassSession("multidimensional array", __LINE__);
 
+$brian = ["Brian", "Mic"];
+$angus = ["name" => "Angus", "instrument" => "Guitar"];
+$instruments = [
+    $brian,
+    $angus
+];
+
+var_dump($instruments);
+
+var_dump([
+    "brian" => $brian,
+    "angus" => $angus
+]);
 
 /**
  * [ array access ] foreach(array as item) || foreach(array as key => value)
  */
 fullStackPHPClassSession("array access", __LINE__);
 
+$acdc = [
+    "band" => "AC/DC",
+    "vocal" => "Brian",
+    "solo-guitar" => "Angus",
+    "base-guitar" => "Malcolm",
+    "bass-guitar" => "Cliff",
+    "drums" => "Phil"
+];
+
+echo "<p>O vocal da banda AC/DC é {$acdc['vocal']} e junto com o {$acdc['solo-guitar']} cantam muito.</p>";
+
+$pearlJam = [
+    "band" => "Pearl Jam",
+    "vocal" => "Eddie",
+    "solo-guitar" => "Mike",
+    "base-guitar" => "Stone",
+    "bass-guitar" => "Jeff",
+    "drums" => "Jack"
+];
+
+$rockBands = [
+    "acdc" => $acdc,
+    "pearl_jam" => $pearlJam
+];
+
+var_dump($rockBands);
+
+echo "<p>{$rockBands['pearl_jam']['vocal']}</p>";
+
+Foreach ($acdc as $item) {
+    echo "<h3>{$item}</h3>";
+}
+
+Foreach ($acdc as $key => $value) {
+    echo "<p>{$value} is a {$key} of band!</p>";
+}
+
+Foreach ($rockBands as $rockBand) {
+    $art = "<article><h1>%s</h1><p>%s</p><p>%s</p><p>%s</p><p>%s</p><p>%s</p></article>";
+    vprintf($art, $rockBand);
+}
+
+Foreach ($rockBands as $rockBand) {
+    echo "<article><h1>{$rockBand['band']}</h1>";
+    Foreach ($rockBand as $role => $name) {
+        if ($role != 'band') {
+            echo "<p><strong>$name:</strong> $role</p>";
+        }
+    }
+}
