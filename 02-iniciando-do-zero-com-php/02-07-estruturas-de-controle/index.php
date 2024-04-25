8f214cf298b87ec51ed0db13f9821fae022a04b4
Cristian0-Pereira <cristianops2020@gmail.com>
2024-04-24 22:09:25 -0300

    ad

--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -9,6 +9,51 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
+$test = false;
+
+if ($test) {
+    var_dump(true);
+} else {
+    var_dump(false);
+}
+
+$age = 51;
+
+if ($age < 20) {
+    var_dump("Bandas Coloridas");
+} elseif ($age > 20 && $age < 50) {
+    var_dump("Ótimas Bandas");
+} else {
+    var_dump("Rock and Roll Raiz");
+}
+
+$hour = 2;
+
+if ($hour >= 5 && $hour < 23) {
+    if ($hour < 7) {
+        var_dump("Bob Marley");
+    } else {
+        var_dump("After Bridge");
+    }
+} else {
+    var_dump("ZzzzzZZZZzzz");
+}
+
+$rock = "";
+
+if (isset($rock)) {
+    var_dump("Rock And Roll!");
+} else {
+    var_dump("Die");
+}
+
+$rockAndRoll = "AC/DC";
+
+if (!empty($rockAndRoll)) {
+    var_dump("Rock existe e toca {$rockAndRoll}");
+} else {
+    var_dump("Não existe ou não está tocando!");
+}
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
