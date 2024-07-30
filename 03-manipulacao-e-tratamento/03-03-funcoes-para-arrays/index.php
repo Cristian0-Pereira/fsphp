b29fc7cad73ce3d1b126995fb3b30bf93686cd09
Cristian0-Pereira <cristianops2020@gmail.com>
2024-07-30 00:23:23 -0300

    ad

--- 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
+++ 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
@@ -7,6 +7,45 @@ fullStackPHPClassName("03.03 - Funções para arrays");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$index = [
+    "AC/DC",
+    "Nirvana",
+    "Alter Bridge"
+];
+
+$assoc = [
+    "band_1" => "AC/DC",
+    "band_2" => "Nirvana",
+    "band_3" => "Alter Bridge",
+];
+
+// Para adicionar itens na variável $index
+array_unshift($index, "","Pearl Jam");
+$assoc = ["band_4" => "Pearl Jam", "band_5" => ""] + $assoc;
+
+array_push($index, "");
+$assoc = $assoc + ["band_6" => ""];
+
+// Para remover o PRIMEIRO item da lista
+array_shift($index);
+array_shift($assoc);
+// Para remover o ÚLTIMO item da lista
+array_pop($index);
+array_pop($assoc);
+
+// Adicionando valor nulo.
+array_unshift($index, "");
+
+// Para eliminar qualquer item q não tiver valor
+$index = array_filter($index);
+$assoc = array_filter($assoc);
+
+var_dump(
+    $index,
+    $assoc
+);
+
+
 
 /*
  * [ ordenação ] reverse | asort | ksort | sort
